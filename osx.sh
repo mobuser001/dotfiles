91542701a690d4d033967fe0e9509df0ed144d60
Robert Brewitz <hello@robertbrewitz.com>
2018-10-17 14:00:35 +0200

    ADDS nvm installer to osx.sh.

--- osx.sh
+++ osx.sh
@@ -30,6 +30,9 @@ rm -rf lolcat
 echo "Installing homebrew"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+echo "Installing nvm"
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+
 echo "Installing figlet"
 brew install figlet
 
