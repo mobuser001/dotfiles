2c1c55bbd1b5a8ac85625c7919caef18201d5781
Robert Brewitz <hello@robertbrewitz.com>
2019-03-06 19:39:10 +0100

    DISABLE lolcat in wsl and chronos for now.

--- chronos.sh
+++ chronos.sh
@@ -9,7 +9,7 @@ ln -sf $ABSOLUTE_PATH/profile $HOME/.profile
 ln -sfn $ABSOLUTE_PATH/vim $HOME/.vim
 ln -sf $ABSOLUTE_PATH/vimrc $HOME/.vimrc
 ln -sf $ABSOLUTE_PATH/vimrc_chronos $HOME/.vimrc_chronos
-ln -sf $ABSOLUTE_PATH/bash_lolcat $HOME/.bash_lolcat
+#ln -sf $ABSOLUTE_PATH/bash_lolcat $HOME/.bash_lolcat
 ln -sf $ABSOLUTE_PATH/tern-project $HOME/.tern-project
 ln -sf $ABSOLUTE_PATH/npmrc $HOME/.npmrc
 ln -sf $ABSOLUTE_PATH/tmux.conf $HOME/.tmux.conf
@@ -38,8 +38,8 @@ crew install fzf
 echo "Installing go"
 crew install go
 
-echo "Installing lolcat"
-gem install lolcat
+#echo "Installing lolcat"
+#gem install lolcat
 
 echo "Installing figlet"
 crew install figlet
--- wsl.sh
+++ wsl.sh
@@ -9,7 +9,7 @@ ln -sf $ABSOLUTE_PATH/profile $HOME/.profile
 ln -sfn $ABSOLUTE_PATH/vim $HOME/.vim
 ln -sf $ABSOLUTE_PATH/vimrc $HOME/.vimrc
 ln -sf $ABSOLUTE_PATH/vimrc_wsl $HOME/.vimrc_wsl
-ln -sf $ABSOLUTE_PATH/bash_lolcat $HOME/.bash_lolcat
+#ln -sf $ABSOLUTE_PATH/bash_lolcat $HOME/.bash_lolcat
 ln -sf $ABSOLUTE_PATH/tern-project $HOME/.tern-project
 ln -sf $ABSOLUTE_PATH/npmrc $HOME/.npmrc
 ln -sf $ABSOLUTE_PATH/tmux.conf $HOME/.tmux.conf
@@ -24,17 +24,17 @@ curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | 
 echo "Installing git-completion"
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o $HOME/.git-completion.bash
 
-echo "Installing rvm and ruby"
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-curl -sSL https://get.rvm.io | bash -s stable --ruby
-
-if gem -v >/dev/null 2>&1; then
-  echo "Installing rainbows"
-  gem install lolcat
-  ln -sf $ABSOLUTE_PATH/bash_lolcat $HOME/.bash_lolcat
-else
-  echo "Failed to install ruby, skipping rainbows"
-fi
+# echo "Installing rvm and ruby"
+# gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+# curl -sSL https://get.rvm.io | bash -s stable --ruby
+# 
+# if gem -v >/dev/null 2>&1; then
+#   echo "Installing rainbows"
+#   gem install lolcat
+#   ln -sf $ABSOLUTE_PATH/bash_lolcat $HOME/.bash_lolcat
+# else
+#   echo "Failed to install ruby, skipping rainbows"
+# fi
 
 echo "Installing the silver searcher (ag)"
 sudo apt install silversearcher-ag -y
