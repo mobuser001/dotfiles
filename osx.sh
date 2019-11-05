c86f4f47a416827ef968488a03de65c8f2e9a6f8
Robert Brewitz <hello@robertbrewitz.com>
2019-11-06 00:14:59 +0100

    REMOVES node installation in osx setup.

--- osx.sh
+++ osx.sh
@@ -38,9 +38,6 @@ if [! -d "$HOME/.nvm"]; then
 fi
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 
-echo "Installing node lts"
-nvm install lts/*
-
 echo "Installing figlet"
 brew install figlet
 
